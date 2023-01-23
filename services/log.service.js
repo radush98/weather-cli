21c60466db3a965202e8ce5487e040af7f4d5aa0
vladymyr <vladymyr.r@radency.com>
2023-01-23 20:24:47 +0200

    added prints

---
+++ services/log.service.js
@@ -0,0 +1,22 @@
+import chalk from "chalk";
+import dedent from "dedent-js";
+
+const printError = (error) => {
+    console.log(chalk.bgRed('ERROR') + ' ' + error);
+};
+
+const printSuccess = (error) => {
+    console.log(chalk.bgGreen('ERROR') + ' ' + error);
+};
+
+const printHelp = () => {
+    console.log(
+        dedent`${chalk.bgCyan('HELP')}
+        Без параметров - вывод погоды'
+        -s [CITY] для утсановки города
+        -h для вывода помощи
+        -t [API_KEY] для сохранения токена
+    `)
+};
+
+export { printError, printSuccess, printHelp };
--- weather.js
+++ weather.js
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
+import { printHelp } from './services/log.service.js';
 
 const initCLI = () => {
     const args = getArgs(process.argv);
-    console.log(args);
-
-    if (args.s) {
-        //help
+    if (args.h) {
+        printHelp();
     }
 
     if (args.s) {
